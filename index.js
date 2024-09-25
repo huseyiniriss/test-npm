9c1c98b3b5c93a8281e9534c8f26d0ab85a81424
Hüseyin İriş <huseyiniriss@gmail.com>
2024-09-26 02:37:36 +0300

    index js update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("NPM projesi başarıyla çalışıyor!");
+const { exec } = require('child_process');
+
+// 'git pull' komutunu çalıştır
+exec('git pull', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Hata oluştu: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Hata çıktı: ${stderr}`);
+        return;
+    }
+    console.log(`Komut çıktısı: ${stdout}`);
+});
