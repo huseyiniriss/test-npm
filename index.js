e205b203b6ae7f4b5f30155c4804d96a9ec6ea9f
Hüseyin İriş <huseyiniriss@gmail.com>
2024-09-26 02:42:09 +0300

    index js update

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
+
 const { exec } = require('child_process');
+const path = require('path');
+
+// GitHub'daki test-npm paketinin olduğu dizine git
+const repoPath = path.resolve(__dirname);
 
 // 'git pull' komutunu çalıştır
-exec('git pull', (error, stdout, stderr) => {
+exec(`git pull https://github.com/huseyiniriss/test-npm.git`, { cwd: repoPath }, (error, stdout, stderr) => {
     if (error) {
         console.error(`Hata oluştu: ${error.message}`);
         return;
     }
     if (stderr) {
-        console.error(`Hata çıktı: ${stderr}`);
+        console.error(`Git uyarısı: ${stderr}`);
         return;
     }
-    console.log(`Komut çıktısı: ${stdout}`);
+    console.log(`Başarılı: ${stdout}`);
 });
